cc6195c21013556f37431cb175a980fbb8d00806
elkarouani <elkarouani@gmail.com>
2021-02-08 17:11:19 +0100

    Ajoutez des fichiers projet.

---
+++ Service Project/Program.cs
@@ -0,0 +1,32 @@
+﻿using Service_Project.Services;
+using System;
+using System.Reflection;
+using Topshelf;
+
+namespace Service_Project
+{
+    class Program
+    {
+        private static string SERVICE_NAME = "HeartbeatService";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Type service_type = Type.GetType($"Service_Project.Services.{SERVICE_NAME}");
+                MethodInfo get_host_factory = service_type.GetMethod(
+                    "GetHostFactory", BindingFlags.Static | BindingFlags.Public);
+
+                TopshelfExitCode exit_code = (TopshelfExitCode)get_host_factory.Invoke(null, null);
+            
+                int exit_code_value = (int)Convert.ChangeType(exit_code, exit_code.GetTypeCode());
+                Environment.ExitCode = exit_code_value;
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine($"{exception.GetType()}: {exception.Message}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Service Project/Services/HeartbeatService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+using Topshelf;
+
+namespace Service_Project.Services
+{
+    public class HeartbeatService
+    {
+        private readonly Timer _timer;
+
+        public HeartbeatService()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        public static TopshelfExitCode GetHostFactory()
+        {
+            return HostFactory.Run(runner => {
+                runner.Service<HeartbeatService>(service =>
+                {
+                    service.ConstructUsing(constructor => new HeartbeatService());
+                    service.WhenStarted(starting => starting.Start());
+                    service.WhenStopped(stopping => stopping.Stop());
+                });
+
+                runner.RunAsLocalSystem();
+
+                runner.SetServiceName("HeartbeatService");
+                runner.SetDisplayName("Heartbeat Service");
+                runner.SetDescription("This is the sample heartbeat service");
+            });
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            Directory.CreateDirectory(@"C:\temp\Demos");
+            File.AppendAllLines(@"C:\temp\Demos\Heartbeat.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
